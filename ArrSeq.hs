0dfce9d7582067c0612e5f80e817f3ffa5f6c2a9
Agustín Mista <mista.agustin@gmail.com>
2015-06-01 23:43:05 -0300

    A lot of code

--- ArrSeq.hs
+++ ArrSeq.hs
@@ -1,15 +1,15 @@
-import qualified Arr as A
-import Arr ((!))
+module ArrSeq where
 
+import Arr
 import Seq
 import Par
 
-instance Seq A.Arr where
-    emptyS      = A.fromList []
+instance Seq Arr where
+    emptyS      = Arr.fromList []
     singletonS  = singletonA
-    lengthS     = A.length
+    lengthS     = Arr.length
     nthS        = (!)
-    tabulateS   = A.tabulate
+    tabulateS   = Arr.tabulate
     mapS        = mapA
     filterS     = filterA
     appendS     = appendA
@@ -17,42 +17,74 @@ instance Seq A.Arr where
     dropS       = dropA
     showtS      = showtA
     showlS      = showlA
-    joinS       = A.flatten
-    reduceS     = undefined
-    scanS       = undefined
-    fromList    = A.fromList
+    joinS       = Arr.flatten
+    reduceS     = reduceA
+    scanS       = scanA
+    fromList    = Arr.fromList
     
-singletonA :: a -> A.Arr a
-singletonA x = A.fromList [x]
+singletonA :: a -> Arr a
+singletonA x = Arr.fromList [x]
 
-mapA :: (a -> b) -> A.Arr a -> A.Arr b
-mapA f a = A.tabulate (\x->(f (a!x))) (A.length a)
+mapA :: (a -> b) -> Arr a -> Arr b
+mapA f a = Arr.tabulate (\x->(f (a!x))) (Arr.length a)
 
-filterA :: (a -> Bool) -> A.Arr a -> A.Arr a
+filterA :: (a -> Bool) -> Arr a -> Arr a
 filterA p a	| size == 0 = emptyS
             | size == 1 = if (p (a!0)) then singletonA (a!0) else emptyS
             | otherwise = appendA filterL filterR
-				where size = A.length a
-				      (filterL, filterR) = (filterA p (takeA a (size `div` 2))) ||| (filterA p (dropA a (size `div` 2)))
+                where size = Arr.length a
+                      (filterL, filterR) = (filterA p (takeA a (size `div` 2))) ||| (filterA p (dropA a (size `div` 2)))
 
 
-appendA :: A.Arr a -> A.Arr a -> A.Arr a
-appendA a b = A.tabulate (\x-> if x < sa then a!x else b!(x-sa)) (sa + sb)
-              where sa = A.length a   
-                    sb = A.length b
+appendA :: Arr a -> Arr a -> Arr a
+appendA a b = Arr.tabulate (\x-> if x < sa then a!x else b!(x-sa)) (sa + sb)
+              where sa = Arr.length a   
+                    sb = Arr.length b
 
-takeA :: A.Arr a -> Int -> A.Arr a
-takeA a s = A.tabulate (\x->(a!x)) s
+takeA :: Arr a -> Int -> Arr a
+takeA a s = Arr.tabulate (\x->(a!x)) s
 
-dropA :: A.Arr a -> Int -> A.Arr a
-dropA a s = A.tabulate (\x->(a!(x + s))) (A.length a - s)
+dropA :: Arr a -> Int -> Arr a
+dropA a s = Arr.tabulate (\x->(a!(x + s))) (Arr.length a - s)
 
-showtA :: A.Arr a -> TreeView a (A.Arr a)
+showtA :: Arr a -> TreeView a (Arr a)
 showtA a | size == 0    = EMPTY
          | size == 1    = ELT (a!0)
          | otherwise    = NODE (takeA a (size `div` 2)) (dropA a (size `div` 2)) 
-            where size = A.length a
+            where size = Arr.length a
             
 showlA :: A.Arr a -> ListView a (A.Arr a)
 showlA a | A.length a == 0  = NIL
          | otherwise        = CONS (a!0) (dropA a 1)
+showlA :: Arr a -> ListView a (Arr a)
+showlA a | Arr.length a == 0  = NIL
+         | otherwise          = CONS (a!0) (dropA a 1)
+
+reduceA :: (a -> a -> a) -> a -> Arr a -> a
+reduceA f e a | size == 0   = e
+              | size == 1   = f e (a!0)
+              | otherwise   = reduceA f e (contract f a)
+                where size = Arr.length a
+
+scanA :: (a -> a -> a) -> a -> Arr a -> (Arr a, a)
+scanA f e a | size == 0     = (emptyS, e)
+            | size == 1     = (singletonA e, f e (a!0))
+            | otherwise     = (combine f a partial, reduced)
+                where size = Arr.length a
+                      (partial, reduced) = scanA f e (contract f a)
+
+
+contract :: (a -> a -> a) -> Arr a -> Arr a
+contract f a | size == 1    = a
+             | even size    = tabulate (\x->f (a!(2*x)) (a!(2*x+1))) half
+             | odd  size    = tabulate (\x->if x == half then (a!(2*x)) else f (a!(2*x)) (a!(2*x+1))) (half+1)
+                    where size = Arr.length a
+                          half = size `div` 2
+                          
+            
+combine :: (a -> a -> a) -> Arr a -> Arr a -> Arr a
+combine f a partial = tabulate aux size
+                    where size   = Arr.length a
+                          aux x  = if even x 
+                                    then (partial!(x `div` 2)) 
+                                    else f (partial!(x `div` 2)) (a!(x-1))
--- ListSeq.hs
+++ ListSeq.hs
@@ -1,6 +1,7 @@
+module ListSeq where
+
 import Seq
 import Par
-import Debug.Trace
 
 instance Seq [] where
     emptyS      = []
@@ -9,7 +10,7 @@ instance Seq [] where
     nthS        = nthL
     tabulateS   = tabulateL
     mapS        = map
-    filterS     = filter
+    filterS     = filterL
     appendS     = (++)
     takeS       = takeL
     dropS       = dropL
@@ -25,26 +26,30 @@ singletonL :: a -> [a]
 singletonL x = [x]
 
 nthL :: [a] -> Int -> a 
-nthL (x:xs) n = if n == 0 
-                then x 
-                else nthL xs (n-1)
+nthL (x:xs) n | n == 0    = x
+              | otherwise = nthL xs (n-1) 
                 
 tabulateL  :: (Int -> a) -> Int -> [a]
 tabulateL f 0 = []
 tabulateL f 1 = [f 0]
 tabulateL f n = (tabulateL f (n-1)) ++ [f (n-1)]
 
+filterL :: (a -> Bool) -> [a] -> [a]
+filterL f [] = []
+filterL f (x:xs) = if b then (x:r) else r
+                        where (b, r) = f x ||| filterL f xs
+                        
 takeL :: [a] -> Int -> [a]
-takeL l n = take n l
+takeL l n = Prelude.take n l
 
 dropL :: [a] -> Int -> [a]
-dropL l n = drop n l
+dropL l n = Prelude.drop n l
 
 showtL :: [a] -> TreeView a [a]
 showtL []   = EMPTY
 showtL [x]  = ELT x
-showtL xs   = NODE  (take ((length xs) `div` 2) (xs))
-                    (drop ((length xs) `div` 2) (xs))
+showtL xs   = NODE (take (size `div` 2) (xs)) (drop (size `div` 2) (xs)) 
+                where size = length xs
 
 showlL :: [a] -> ListView a [a]
 showlL [] = NIL
@@ -57,21 +62,21 @@ joinL (x:xs) = x ++ joinL xs
 reduceL :: (a -> a -> a) -> a -> [a] -> a
 reduceL f e []       = e
 reduceL f e [x]      = f e x 
-reduceL f e (x:y:xs) = reduceL f e ((f x y) : xs)
+reduceL f e xs = reduceL f e (contract f xs)
 
 scanL :: (a -> a -> a) -> a -> [a] -> ([a], a)
 scanL f e []  = ([], e)
 scanL f e [x] = ([e], f e x)
 scanL f e xs  = (combine True f xs partial, reduced)
-                  where (partial, reduced) = scanL f e (reduceStep f xs)
+                  where (partial, reduced) = scanL f e (contract f xs)
 
-reduceStep :: (a -> a -> a) -> [a] -> [a]
-reduceStep f []  = []
-reduceStep f [x] = [x]
-reduceStep f (x:y:xs) = (f x y) : (reduceStep f xs)
+contract :: (a -> a -> a) -> [a] -> [a]
+contract f []  = []
+contract f [x] = [x]
+contract f (x:y:xs) = (f x y) : (contract f xs)
 
 combine :: Bool -> (a -> a -> a) -> [a] -> [a] -> [a]
 combine _ _ [] _ = []
-combine True  f s@(_:_)   c@(x:_)   = x : (combine False f s c )
+combine True  f s@(_:_)   c@(x:_)   = x : (combine False f s c)
 combine False f [_]       (_:_)     = []
-combine False f (x:_:xs)  (x':xs')  = (f x' x) : (combine True f xs xs' )
+combine False f (x:_:xs)  (x':xs')  = (f x' x) : (combine True f xs xs')
